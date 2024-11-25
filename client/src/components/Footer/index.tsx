8b682e3610245b7df379f0041a2bad1d488f9edf
demuelle <dmueller@trilogyed.com>
2024-11-24 23:50:23 -0600

    Updates footer

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -3,16 +3,7 @@ const Footer = () => {
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container text-center mb-5">
         <h4>
-          Made with{' '}
-          <span
-            className="emoji"
-            role="img"
-            aria-label="heart"
-            aria-hidden="false"
-          >
-            ❤️
-          </span>{' '}
-          by the Word Guess team.
+          Made with a red heart icon by the Word Guess team.
         </h4>
       </div>
     </footer>
