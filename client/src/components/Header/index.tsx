6918422bd6f7d7eb5d123a0fae1e5eaaa1c86f28
demuelle <dmueller@trilogyed.com>
2024-11-26 12:11:14 -0600

    Kicks off full approval workflow

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -2,8 +2,9 @@ const Header = () => {
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1 className="m-0">Movie Guess</h1>
+        <h1 className="m-0">Bollywood Only! Jkjk</h1>
         <p className="m-0">Guess the movie title or else!</p>
+        <p>Deployed Via CircleCI</p>
       </div>
     </header>
   );
